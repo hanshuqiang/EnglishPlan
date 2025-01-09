70fe62df6e8e312f9ef65e8d460dc8fe66f8d28b
hanshuqiang <hanshuqiang@gmail.com>
2025-01-09 16:46:33 +0800

    i

--- astro.config.mjs
+++ astro.config.mjs
@@ -33,7 +33,7 @@ const oklchToHex = (str) => {
 // https://astro.build/config
 export default defineConfig({
   site: "https://fuwari.vercel.app/",
-  base: "/myblog",
+  base: "/EnglishPlan",
   trailingSlash: "always",
   integrations: [
     tailwind(),
